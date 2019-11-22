6771a3c6d294ee8edff251d309e8d37209b07bf7
Tom Hill <tom.hill991@gmail.com>
2019-11-22 13:06:50 +0800

    compiling error when pushing. Retrying

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root_to "cocktails#index"
+  route_to "cocktails#index"
   resources :cocktails, only: [:new, :create, :index, :show, :destroy] do
     resources :doses, only: [:new, :create]
   end
