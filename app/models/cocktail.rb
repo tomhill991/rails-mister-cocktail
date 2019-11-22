c6861975883394681bd9280ba5fa547dc55b000b
Tom Hill <tom.hill991@gmail.com>
2019-11-22 15:26:59 +0800

    added CSS to forms. Tried to make ingredients page look pretty. So far have failed

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,6 +1,6 @@
 class Cocktail < ApplicationRecord
   mount_uploader :photo, PhotoUploader
   validates :name, uniqueness: true, presence: true
-  # has_many :doses, dependent: :destroy
+  has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
 end
